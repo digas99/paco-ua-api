4fe6ad91e6cef69e02a33be9d4924d0e6b9a0cc0
Diogo Correia <diogo.correia99@ua.pt>
2022-08-21 00:24:40 +0100

    added swagger /expiration

--- routes/expiration.js
+++ routes/expiration.js
@@ -10,6 +10,25 @@ const setup = {
     "key": "expiration"
 }
 
+/**
+ * @swagger
+ * /expiration:
+ *  get:
+ *      summary: Returns a list of years with their expiration status
+ *      tags: [Expiration]
+ *      security:
+ *          - basicAuth: []
+ *      responses:
+ *          200:
+ *              description: List of years with their expiration status
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Expiration'
+ */
+
 router.get("/", async (req, res) => {
     handleResponse(req, res, paco.expiration, setup);
 });
